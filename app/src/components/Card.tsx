863c98359b4939930a4ffe774a9d0e3e2aa3b6b8
ArtemHard <95387501+ArtemHard@users.noreply.github.com>
2022-12-26 02:35:20 +0300

    add card info

--- app/src/components/Card.tsx
+++ app/src/components/Card.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 const CardWrapper = styled.div`
@@ -7,12 +8,29 @@ const CardWrapper = styled.div`
   width: 560px; // FIT CONTENT
   height: 100px;
   display: flex;
-  flex-direction: column;
+  /* flex-wrap: nowrap; */
+  /* align-items: center; */
+  justify-content: space-between;
   border-radius: var(--rad-b);
 `;
 
-const InfoWrapper = styled.div`
+const InfoLeftContainer = styled.div`
   display: flex;
+  flex-direction: column-reverse;
+  justify-content: space-between;
+  padding: 1.5%;
+  /* align-items: flex-end; */
+`;
+
+const InfoRightContainer = styled.div`
+  display: flex;
+  flex-direction: column-reverse;
+  padding: 1.5%;
+  /* justify-content: flex-end; */
+`;
+
+const InfoWrapper = styled.div`
+  /* padding-bottom: 5%; */
 `;
 
 const InfoArticle = styled.span`
@@ -37,13 +55,37 @@ const InfoData = styled.span`
   color: var(--color-text);
 `;
 
+const Details = styled.span`
+  font-family: var(--family);
+  font-style: normal;
+  font-weight: var(--fw-normal);
+  font-size: var(--fs-m);
+  line-height: 14px;
+  color: #4b51ef;
+`;
+
 export const Card = () => {
   return (
     <CardWrapper>
-      <InfoWrapper>
-        <InfoArticle>ФИО: </InfoArticle>
-        <InfoData> ВАСЯ ИВАНОВ</InfoData>
-      </InfoWrapper>
+      <InfoLeftContainer>
+        <InfoWrapper>
+          <InfoArticle>ФИО: </InfoArticle>
+          <InfoData> ВАСЯ ИВАНОВ</InfoData>
+        </InfoWrapper>
+        <InfoWrapper>
+          <InfoArticle>ФИО: </InfoArticle>
+          <InfoData> ВАСЯ ИВАНОВ</InfoData>
+        </InfoWrapper>
+        <InfoWrapper>
+          <InfoArticle>ФИО: </InfoArticle>
+          <InfoData> ВАСЯ ИВАНОВ</InfoData>
+        </InfoWrapper>
+      </InfoLeftContainer>
+      <InfoRightContainer>
+        <Details>
+          <a href='/'>Подробнее</a>
+        </Details>
+      </InfoRightContainer>
     </CardWrapper>
   );
 };
--- app/src/index.tsx
+++ app/src/index.tsx
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App";
 import { store } from "./redux/store";
 import { Provider } from "react-redux";
+import { GlobalStyle } from "./styles/global";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -11,6 +12,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Provider store={store}>
+      <GlobalStyle />
       <App />
     </Provider>
   </React.StrictMode>
